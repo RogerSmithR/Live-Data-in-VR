ad5ac661130952a23dfcb1edb04bd911faab2a97
Roger Smith Rivas <roger.smith.rivas@hotmail.com>
2023-04-17 23:09:15 -0400

    Adding Live Data Transmitter API
    Asp .Net Core API

---
+++ LiveDataTransmiterAPI/Controllers/HeatValue.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LiveDataTransmiterAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class heatvalue : ControllerBase
+    {
+        [HttpGet(Name = "GetHeatValue")]
+        public string Get()
+        {
+            Random rnd = new Random();
+            return rnd.Next(0, 100).ToString();
+        }
+    }
+}
